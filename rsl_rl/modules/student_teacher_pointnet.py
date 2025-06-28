fdefc87da6226b30494954ef520e7028fab93f6e
RodsCoimbra <rodscoimbra@gmail.com>
2025-06-28 17:48:33 +0100

    Add StudentTeacherPointNet class, need to add to the on_policy_runner for scaling correctly

--- rsl_rl/modules/student_teacher_pointnet.py
+++ rsl_rl/modules/student_teacher_pointnet.py
@@ -9,63 +9,59 @@ import torch
 import torch.nn as nn
 from torch.distributions import Normal
 
-class ActorCriticPointNet(nn.Module):
+class StudentTeacherPointNet(nn.Module):
     is_recurrent = False
 
     def __init__(
         self,
-        num_actor_obs,
-        num_critic_obs,
+        num_student_obs,
+        num_teacher_obs,
         num_actions,
-        init_noise_std=1.0,
-        noise_std_type: str = "scalar",
+        student_hidden_dims=[256, 128, 64],
+        activation="elu", 
+        init_noise_std=0.1,
         proprioception_space = None,
         latent_dim: int = 32,
         **kwargs,
     ):
         if kwargs:
             print(
-                "ActorCriticPointNet.__init__ got unexpected arguments, which will be ignored: "
+                "StudentTeacherPointNet.__init__ got unexpected arguments, which will be ignored: "
                 + str([key for key in kwargs.keys()])
             )
         super().__init__()
+
+        self.loaded_teacher = False  # indicates if teacher has been loaded
+
+        mlp_input_dim_s = num_student_obs
         
-        if proprioception_space is None:
-            raise ValueError("proprioception_space must be provided")
-        critic_space = num_actor_obs - num_critic_obs
-        
-        # Policy
-        self.actor = AgentPointNetMLP(proprioception_space, latent_dim, num_actions)
+        # student
+        student_layers = []
+        student_layers.append(nn.Linear(mlp_input_dim_s, student_hidden_dims[0]))
+        student_layers.append(activation)
+        for layer_index in range(len(student_hidden_dims)):
+            if layer_index == len(student_hidden_dims) - 1:
+                student_layers.append(nn.Linear(student_hidden_dims[layer_index], num_actions))
+            else:
+                student_layers.append(nn.Linear(student_hidden_dims[layer_index], student_hidden_dims[layer_index + 1]))
+                student_layers.append(activation)
+        self.student = nn.Sequential(*student_layers)
 
-        # Value function
-        self.critic = AgentPointNetMLP(proprioception_space + critic_space, latent_dim, 1)
+        # teacher
+        self.teacher = AgentPointNetMLP(proprioception_space, latent_dim, num_actions)
+        self.teacher.eval()
 
-        print(f"Actor MLP: {self.actor}")
-        print(f"Critic MLP: {self.critic}")
 
-        # Action noise
-        self.noise_std_type = noise_std_type
-        if self.noise_std_type == "scalar":
-            self.std = nn.Parameter(init_noise_std * torch.ones(num_actions))
-        elif self.noise_std_type == "log":
-            self.log_std = nn.Parameter(torch.log(init_noise_std * torch.ones(num_actions)))
-        else:
-            raise ValueError(f"Unknown standard deviation type: {self.noise_std_type}. Should be 'scalar' or 'log'")
+        print(f"Student MLP: {self.student}")
+        print(f"Teacher MLP: {self.teacher}")
 
-        # Action distribution (populated in update_distribution)
+        # action noise
+        self.std = nn.Parameter(init_noise_std * torch.ones(num_actions))
         self.distribution = None
         # disable args validation for speedup
-        Normal.set_default_validate_args(False)
+        Normal.set_default_validate_args = False
 
-    @staticmethod
-    # not used at the moment
-    def init_weights(sequential, scales):
-        [
-            torch.nn.init.orthogonal_(module.weight, gain=scales[idx])
-            for idx, module in enumerate(mod for mod in sequential if isinstance(mod, nn.Linear))
-        ]
-
-    def reset(self, dones=None):
+    def reset(self, dones=None, hidden_states=None):
         pass
 
     def forward(self):
@@ -84,35 +80,25 @@ class ActorCriticPointNet(nn.Module):
         return self.distribution.entropy().sum(dim=-1)
 
     def update_distribution(self, observations):
-        # compute mean
-        mean = self.actor(observations)
-        # compute standard deviation
-        if self.noise_std_type == "scalar":
-            std = self.std.expand_as(mean)
-        elif self.noise_std_type == "log":
-            std = torch.exp(self.log_std).expand_as(mean)
-        else:
-            raise ValueError(f"Unknown standard deviation type: {self.noise_std_type}. Should be 'scalar' or 'log'")
-        # create distribution
+        mean = self.student(observations)
+        std = self.std.expand_as(mean)
         self.distribution = Normal(mean, std)
 
-    def act(self, observations, **kwargs):
+    def act(self, observations):
         self.update_distribution(observations)
         return self.distribution.sample()
 
-    def get_actions_log_prob(self, actions):
-        return self.distribution.log_prob(actions).sum(dim=-1)
-
     def act_inference(self, observations):
-        actions_mean = self.actor(observations)
+        actions_mean = self.student(observations)
         return actions_mean
 
-    def evaluate(self, critic_observations, **kwargs):
-        value = self.critic(critic_observations)
-        return value
+    def evaluate(self, teacher_observations):
+        with torch.no_grad():
+            actions = self.teacher(teacher_observations)
+        return actions
 
     def load_state_dict(self, state_dict, strict=True):
-        """Load the parameters of the actor-critic model.
+        """Load the parameters of the student and teacher networks.
 
         Args:
             state_dict (dict): State dictionary of the model.
@@ -121,11 +107,38 @@ class ActorCriticPointNet(nn.Module):
 
         Returns:
             bool: Whether this training resumes a previous training. This flag is used by the `load()` function of
-                  `OnPolicyRunner` to determine how to load further parameters (relevant for, e.g., distillation).
+                  `OnPolicyRunner` to determine how to load further parameters.
         """
 
-        super().load_state_dict(state_dict, strict=strict)
-        return True
+        # check if state_dict contains teacher and student or just teacher parameters
+        if any("actor" in key for key in state_dict.keys()):  # loading parameters from rl training
+            # rename keys to match teacher and remove critic parameters
+            teacher_state_dict = {}
+            for key, value in state_dict.items():
+                if "actor." in key:
+                    teacher_state_dict[key.replace("actor.", "")] = value
+            self.teacher.load_state_dict(teacher_state_dict, strict=strict)
+            # also load recurrent memory if teacher is recurrent
+            if self.is_recurrent and self.teacher_recurrent:
+                raise NotImplementedError("Loading recurrent memory for the teacher is not implemented yet")  # TODO
+            # set flag for successfully loading the parameters
+            self.loaded_teacher = True
+            self.teacher.eval()
+            return False
+        elif any("student" in key for key in state_dict.keys()):  # loading parameters from distillation training
+            super().load_state_dict(state_dict, strict=strict)
+            # set flag for successfully loading the parameters
+            self.loaded_teacher = True
+            self.teacher.eval()
+            return True
+        else:
+            raise ValueError("state_dict does not contain student or teacher parameters")
+
+    def get_hidden_states(self):
+        return None
+
+    def detach_hidden_states(self, dones=None):
+        pass
 
 
 class AgentPointNetMLP(nn.Module):
